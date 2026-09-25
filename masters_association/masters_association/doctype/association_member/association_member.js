598f06e8fd3d78aea5b33129a1b271782ed3c6f5
Mujeeb Cpy <mujeebcpy@gmail.com>
2026-09-25 22:21:45 +0530

    auto fetch GST Details

--- masters_association/masters_association/doctype/association_member/association_member.js
+++ masters_association/masters_association/doctype/association_member/association_member.js
@@ -1,8 +1,39 @@
 // Copyright (c) 2026, mujeebcpy and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Association Member", {
-// 	refresh(frm) {
+frappe.ui.form.on("Association Member", {
+	gst(frm) {
+		const gstin_pattern = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][1-9A-Z]Z[0-9A-Z]$/;
+		const gstin = (frm.doc.gst || "").trim().toUpperCase();
 
-// 	},
-// });
+		if (gstin !== frm.doc.gst) {
+			frm.set_value("gst", gstin);
+		}
+
+		if (!gstin) {
+			frm.set_value("gst_company_name", "");
+			frm.set_value("gst_address", "");
+			frm.set_value("gst_pincode", "");
+			frm.set_value("gst_state", "");
+			frm.set_value("gst_status", "");
+			return;
+		}
+
+		if (!gstin_pattern.test(gstin)) return;
+
+		frappe.call({
+			method: "masters_association.api.fetch_gst_details",
+			args: { gstin },
+			freeze: true,
+			freeze_message: __("Fetching GST details..."),
+			callback: function(r) {
+				if (!r.message) return;
+				frm.set_value("gst_company_name", r.message.gst_company_name);
+				frm.set_value("gst_address", r.message.gst_address);
+				frm.set_value("gst_pincode", r.message.gst_pincode);
+				frm.set_value("gst_state", r.message.gst_state);
+				frm.set_value("gst_status", r.message.gst_status);
+			},
+		});
+	},
+});
--- masters_association/masters_association/web_form/members_registration/members_registration.js
+++ masters_association/masters_association/web_form/members_registration/members_registration.js
@@ -2,6 +2,49 @@ frappe.ready(function() {
 	// Basic format for a regular PAN-based GSTIN.
 	const gstin_pattern = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z][1-9A-Z]Z[0-9A-Z]$/;
 
+	function clear_gst_details() {
+		frappe.web_form.set_value("gst_company_name", "");
+		frappe.web_form.set_value("gst_address", "");
+		frappe.web_form.set_value("gst_pincode", "");
+		frappe.web_form.set_value("gst_state", "");
+		frappe.web_form.set_value("gst_status", "");
+	}
+
+	function fetch_gst_details(gstin) {
+		frappe.call({
+			method: "masters_association.api.fetch_gst_details",
+			args: { gstin },
+			callback: function(r) {
+				if (!r.message) return;
+				frappe.web_form.set_value("gst_company_name", r.message.gst_company_name);
+				frappe.web_form.set_value("gst_address", r.message.gst_address);
+				frappe.web_form.set_value("gst_pincode", r.message.gst_pincode);
+				frappe.web_form.set_value("gst_state", r.message.gst_state);
+				frappe.web_form.set_value("gst_status", r.message.gst_status);
+			}
+		});
+	}
+
+	// Auto-fetch GST details as soon as a valid-looking GSTIN is entered.
+	frappe.web_form.on("gst", (field, value) => {
+		const gstin = (value || "").trim().toUpperCase();
+
+		if (gstin !== value) {
+			frappe.web_form.set_value("gst", gstin);
+		}
+
+		if (!gstin) {
+			clear_gst_details();
+			return;
+		}
+
+		if (gstin_pattern.test(gstin)) {
+			fetch_gst_details(gstin);
+		} else {
+			clear_gst_details();
+		}
+	});
+
 	frappe.web_form.validate = () => {
 		const gstin = (frappe.web_form.get_value("gst") || "")
 			.trim()
